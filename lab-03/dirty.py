065074757785d65bbb5f42a3d118d7123ae06c68
aleneus <aleneus@gmail.com>
2020-04-11 23:38:58 +0300

    add lab about pylint

---
+++ lab-03/dirty.py
@@ -0,0 +1,18 @@
+import numpy
+import random
+
+default_accuracy = 3
+
+
+def sumTwoValues(first, second):
+    return first + second
+
+
+def div(x, y, ACCURACY, module):
+    return round(x/y,  ACCURACY)
+
+def main():
+    pass
+
+
+main()
