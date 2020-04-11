6f8f4a9b99501496e0e2b87a6ad74aa759eaadcd
aleneus <aleneus@gmail.com>
2020-04-11 23:48:39 +0300

    expand pylint code example

--- lab-03/dirty.py
+++ lab-03/dirty.py
@@ -1,5 +1,6 @@
-import numpy
+import numpy as np
 import random
+import sqlite3
 
 default_accuracy = 3
 
@@ -10,6 +11,15 @@ def sumTwoValues(first, second):
 
 def div(x, y, ACCURACY, module):
     return round(x/y,  ACCURACY)
+
+
+
+def get_rand():
+    return random.randint(1, 10)
+
+def rand_array():
+    a = [get_rand(), get_rand(), get_rand()]
+    return np.array(a)
 
 def main():
     pass
